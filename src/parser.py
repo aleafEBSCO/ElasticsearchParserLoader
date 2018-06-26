5c301711367d1c93a82bca854e3e25492b97c5b7
Andrew Leaf <aleaf@ebsco.com>
2018-06-26 16:19:52 -0400

    Added code to go through json to save paths and types

--- src/parser.py
+++ src/parser.py
@@ -1,12 +1,50 @@
 import os
 import json
 
-directory = os.fsencode("../records")
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
+
+def recurseObject(obj, curPath, paths):
+    if type(obj) is not dict:
+        if type(obj) is list:
+            for item in obj:
+                if type(item) is dict:
+                    for key in item.keys():
+                        recurseObject(item[key], (curPath + key + "/"), paths)
+                else:
+                    if curPath in paths:
+                        paths[curPath].add(type(item))
+                    else:
+                        paths[curPath] = set([type(item)])
+
+                    #print(curPath + ": ", end="")
+                    #print(type(obj))
+        else:
+            if curPath in paths:
+                paths[curPath].add(type(obj))
+            else:
+                paths[curPath] = set([type(obj)])
+            #print(curPath + ": ", end="")
+            #print(type(obj))
+    else:
+        for key in obj.keys():
+            recurseObject(obj[key], (curPath + key + "/"), paths)
+
+    return paths
+
+
+finalPaths = dict();
+
+for filename in os.listdir("../records"):
 
     openFile = "../records" + "/" + filename
     if openFile.endswith(".json"):
         with open(openFile) as f:
             data = json.load(f)
-        print(data['contents'])
+        recurseObject(data, "", finalPaths)
+        #print(data['contents'])
+
+for key in finalPaths.keys():
+    print(key + ": ", end="")
+    for item in finalPaths[key]:
+        print(item, end="")
+        print(", ", end="")
+    print()
