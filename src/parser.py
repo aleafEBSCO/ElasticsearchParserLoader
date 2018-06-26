5261b7c5b2648c135bd59b43b25a9a7a1449fdbb
Graham Preston <gpreston@ebsco.com>
2018-06-26 16:48:58 -0400

    Renaming and main function

--- src/parser.py
+++ src/parser.py
@@ -2,49 +2,52 @@ import os
 import json
 
 
-def recurseObject(obj, curPath, paths):
+def main(records_dir):
+    final_paths = {}
+
+    for filename in os.listdir(records_dir):
+        path = records_dir + filename
+        if path.endswith(".json"):
+            with open(path) as f:
+                data = json.load(f)
+            traverse(data, "", final_paths)
+
+    for key in final_paths:
+        print(key + ": ", end="")
+        for item in final_paths[key]:
+            print(item, end="")
+            print(", ", end="")
+        print()
+
+
+def traverse(obj, path, all_paths):
     if type(obj) is not dict:
         if type(obj) is list:
             for item in obj:
                 if type(item) is dict:
                     for key in item.keys():
-                        recurseObject(item[key], (curPath + key + "/"), paths)
+                        traverse(item[key], (path + key + "/"), all_paths)
                 else:
-                    if curPath in paths:
-                        paths[curPath].add(type(item))
+                    if path in all_paths:
+                        all_paths[path].add(type(item))
                     else:
-                        paths[curPath] = set([type(item)])
+                        all_paths[path] = {type(item)}
 
-                    #print(curPath + ": ", end="")
-                    #print(type(obj))
+                    # print(curPath + ": ", end="")
+                    # print(type(obj))
         else:
-            if curPath in paths:
-                paths[curPath].add(type(obj))
+            if path in all_paths:
+                all_paths[path].add(type(obj))
             else:
-                paths[curPath] = set([type(obj)])
-            #print(curPath + ": ", end="")
-            #print(type(obj))
+                all_paths[path] = {type(obj)}
+            # print(curPath + ": ", end="")
+            # print(type(obj))
     else:
         for key in obj.keys():
-            recurseObject(obj[key], (curPath + key + "/"), paths)
+            traverse(obj[key], (path + key + "/"), all_paths)
 
-    return paths
+    return all_paths
 
 
-finalPaths = dict();
-
-for filename in os.listdir("../records"):
-
-    openFile = "../records" + "/" + filename
-    if openFile.endswith(".json"):
-        with open(openFile) as f:
-            data = json.load(f)
-        recurseObject(data, "", finalPaths)
-        #print(data['contents'])
-
-for key in finalPaths.keys():
-    print(key + ": ", end="")
-    for item in finalPaths[key]:
-        print(item, end="")
-        print(", ", end="")
-    print()
+if __name__ == '__main__':
+    main('../records/')
