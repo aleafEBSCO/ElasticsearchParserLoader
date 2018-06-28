81756590322d44331e85b88a9af045245042ab72
Andrew Leaf <aleaf@ebsco.com>
2018-06-28 15:53:28 -0400

    Created comparison table

--- src/parser.py
+++ src/parser.py
@@ -16,6 +16,7 @@ def main(records_dir):
     for d in data:
         schema = compute_schema(d, schema)
     pprint(schema)
+    return schema
 
 
 # Compute the overall schema given a current schema
@@ -52,4 +53,33 @@ def parser_type(o):
 
 
 if __name__ == '__main__':
-    main('../records/')
+    returnedSchema = main('../records/')
+    #pretend the below schema is what was returned by the above function
+    #returnedSchema = {'contributors': {'schema': {'authors': {'schema': {'firstname': {'schema': {}, 'classes': {<class 'str'>}}, 'lastname': {'schema': {}, 'classes': {<class 'str'>}}}, 'classes': {<class 'list'>}}, 'test': {'schema': {}, 'classes': {<class 'str'>}}}, 'classes': set()}, 'date': {'schema': {'day': {'schema': {}, 'classes': {<class 'int'>}}, 'month': {'schema': {}, 'classes': {<class 'int'>}}, 'year': {'schema': {}, 'classes': {<class 'int'>}}}, 'classes': set()}, 'contents': {'schema': {}, 'classes': {<class 'str'>, <class 'int'>}}, 'publisher': {'schema': {}, 'classes': {<class 'str'>}}}
+    print(returnedSchema)
+
+    '''
+    Notes:
+    Key: * means the value is optional
+
+            String | Integer | Float | Object | Array | Boolean | null
+    String |       | String  | String| Array  | Array | Array   | String*
+    Integer|String |         | Float | Array  | Array | Array   | Integer*
+    Float  |String | Float   |       | Array  | Array | Array   | Float*
+    Object |Array  | Array   | Array |        | Array | Array   | Object*
+    Array  |Array  | Array   | Array | Array  |       | Array   | Array*
+    Boolean|Array  | Array   | Array | Array  | Array |         | Boolean*
+    null   |String*| Integer*| Float*| Object*| Array*| Boolean*| 
+
+    '''
+
+    #create a hash table like the one above
+    table = {};
+    #create the rows
+    table["str"] = {"int": "str", "float": "str", "dict": "list", "list": "list", "bool": "list", "null": "str"}
+    table["int"] = {"str": "str", "float": "float", "dict": "list", "list": "list", "bool": "list", "null": "int"}
+    table["float"] = {"str": "str", "int": "float", "dict": "list", "list": "list", "bool": "list", "null": "float"}
+    table["dict"] = {"str": "list", "int": "list", "float": "list", "list": "list", "bool": "list", "null": "dict"}
+    table["list"] = {"str": "list", "int": "list", "float": "list", "dict": "list", "bool": "list", "null": "list"}
+    table["bool"] = {"str": "list", "int": "list", "float": "list", "dict": "list", "list": "list", "null": "bool"}
+    table["null"] = {"str": "str", "int": "int", "float": "float", "dict": "dict", "list": "list", "bool": "bool"}
